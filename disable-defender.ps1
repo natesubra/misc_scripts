3b0ef9a79ef0c35481c8c84add2a5d1f3e61775b
Nate Subra <12102127+natesubra@users.noreply.github.com>
2023-09-25 18:01:48 -0500

    additional warning

--- disable-defender.ps1
+++ disable-defender.ps1
@@ -5,6 +5,8 @@ param ()
 
 # "Disables" Defender by adding exclusions and turning off advanced bits. Run this under an elevated powershell prompt
 # Defender will be essentially gutted/disabled without messing with any files/underlying services. Windows Security center will still display that AV is working etc.
+
+# WARNING: This is intended to be run on fully patched machines. It will fail if run on older versions of Windows 10/11.
 # WARNING: This is intended to work on RE/Malware research machines and it's ability to alter MDE configurations will depend on how MDE policy is configured. 
 # WARNING: TL;DR This is not intended to be run on a managed device.
 
